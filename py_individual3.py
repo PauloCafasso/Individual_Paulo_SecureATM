1ef054601cdaf96445eff920981e7b06b2119415
PauloCafasso <paulo.cafasso@sptech.school>
2023-12-04 22:08:10 -0300

    python conectado com sql server

--- py_individual3.py
+++ py_individual3.py
@@ -30,50 +30,50 @@ if resposta == "s":
         if mydb.is_connected():
                 print("Banco conectado")
 
-                ram_percent = psutil.virtual_memory().percent
-                data_atual = date.today()
+                if sql_server_cnx.is_connected():
+                    print("Server conectado")
+
+                    ram_percent = psutil.virtual_memory().percent
+                    data_atual = date.today()
 
                 # Formate a data no formato apropriado para MySQL (YYYY-MM-DD)
-                data_formatada = data_atual.strftime('%Y-%m-%d')
+                    data_formatada = data_atual.strftime('%Y-%m-%d')
 
                 # Obtenha a hora atual
-                hora_atual = datetime.now()
+                    hora_atual = datetime.now()
 
                 # Formate a hora no formato apropriado para MySQL (HH:MM:SS)
-                hora_formatada = hora_atual.strftime('%H:%M:%S')
+                    hora_formatada = hora_atual.strftime('%H:%M:%S')
 
                 # Junte a data e a hora formatadas
-                data_hora_formatada = '{} {}'.format(data_formatada, hora_formatada)
+                    data_hora_formatada = '{} {}'.format(data_formatada, hora_formatada)
 
                 # Lista de variáveis que carregam os dados colhidos que serão inseridos nessa iteração EM ORDEM
-                dados_insert = [data_hora_formatada, ram_percent, 1, fk_atm, 3]
-
-                # Retorna a versão atual do SQL / infos de sistema do banco
-                db_info = mydb.get_server_info()
+                    dados_insert = [data_hora_formatada, ram_percent, 1, fk_atm, 3]
                 
                 # Abre cursor permitindo inserção
-                mycursor = mydb.cursor()
-                cursor_sql_server = sql_server_cnx.cursor()
+                    mycursor = mydb.cursor()
+                    cursor_sql_server = sql_server_cnx.cursor()
 
                 # Inserção stringificada
-                sql_query = "INSERT INTO leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (%s,%s,%s,%s,%s)"
-                sql_server_query="INSERT INTO leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (%s,%s,%s,%s,%s)"
+                    sql_query = "INSERT INTO leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (%s,%s,%s,%s,%s)"
+                    sql_server_query="INSERT INTO leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (%s,%s,%s,%s,%s)"
 
                 # Cursor executa a query e é feito o commit da transação
-                mycursor.execute(sql_query, dados_insert)
-                cursor_sql_server.execute(sql_server_query, dados_insert)
+                    mycursor.execute(sql_query, dados_insert)
+                    cursor_sql_server.execute(sql_server_query, dados_insert)
                                   
                 # Commit para efetivar a transação
-                mydb.commit()
-                sql_server_cnx.commit()
+                    mydb.commit()
+                    sql_server_cnx.commit()
                 # Fecha cursor e conexão idependente do sucesso na inserção
-                if mydb.is_connected():
-                    mycursor.close()
+                    if mydb.is_connected():
+                        mycursor.close()
 
                 # Intervalo de 4 segundos
-                time.sleep(4)
+                    time.sleep(4)
                 
-                if mydb.is_connected():
-                    mydb.close()
+                    if mydb.is_connected():
+                        mydb.close()
 else:
     print("Programa encerrado.")
